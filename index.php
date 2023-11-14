424e35a15ae1998165ff8220f4c63fa8d436278c
Vitaliy <organizm78by@gmail.com>
2023-11-14 22:43:50 +0300

    edit gitignore

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-//echo '<pre>';
-//print_r(gd_info());
-//echo '</pre>';
-//
-//exit();
-
-
 // Version
 define('VERSION', '4.0.2.3');
 
@@ -16,10 +9,10 @@ if (is_file('config.php')) {
 }
 
 // Install
-if (!defined('DIR_APPLICATION')) {
-	header('Location: install/index.php');
-	exit();
-}
+//if (!defined('DIR_APPLICATION')) {
+//	header('Location: install/index.php');
+//	exit();
+//}
 
 // Startup
 require_once(DIR_SYSTEM . 'startup.php');
