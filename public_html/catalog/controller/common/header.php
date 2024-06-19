cbca281ce0c86cb17087dc461198d22f81435761
Vitaliy <organizm78by@gmail.com>
2024-06-19 19:24:54 +0300

    Публичная часть->шапка: первый рефакторинг

--- public_html/catalog/controller/common/header.php
+++ public_html/catalog/controller/common/header.php
@@ -69,7 +69,13 @@ class ControllerCommonHeader extends Controller {
 		$data['shopping_cart'] = $this->url->link('checkout/cart');
 		$data['checkout'] = $this->url->link('checkout/checkout', '', true);
 		$data['contact'] = $this->url->link('information/contact');
+        $data['address'] = nl2br($this->config->get('config_address'));
 		$data['telephone'] = $this->config->get('config_telephone');
+        $data['telephone_clear'] = preg_replace('/[^\d]+/ui', '', $this->config->get('config_telephone'));
+        $data['telephone_second'] = $this->config->get('config_telephone_second');
+        $data['telephone_second_clear'] = preg_replace('/[^\d]+/ui', '', $this->config->get('config_telephone_second'));
+
+        $data['email'] = $this->config->get('config_email');
 		
 		$data['language'] = $this->load->controller('common/language');
 		$data['currency'] = $this->load->controller('common/currency');
